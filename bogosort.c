ff3b676f14c7c0f0f17609d245ed5be1ea1ef2e4
jdanray <daniel.ray318@gmail.com>
2014-08-13 01:37:35 -0500

    Update bogosort.c

--- bogosort.c
+++ bogosort.c
@@ -6,17 +6,15 @@ int main() {
 	int arr[2] = {3,1};
 	int order[2] = {1,3};
 	int len = sizeof(arr) / sizeof(arr[0]);
-        if (len > 1) 
-        {
-		while (arr != order) 
+	while (arr != order) 
+	{
+		//Shuffle
+		for (int i = len - 1; i > 0; i--) 
 		{
-			for (int i = len - 1; i > 0; i--) 
-			{
-				int temp = arr[i];
-				int r = rand() % (i + 1);
-				arr[i] = arr[r];
-				arr[r] = temp;
-			}        
+			int temp = arr[i];
+			int r = rand() % (i + 1);
+			arr[i] = arr[r];
+			arr[r] = temp;
 		}
 	} 
 }
