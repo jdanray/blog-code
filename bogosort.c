731f9b81f59afaa21d71c2623782995362dc00dd
jdanray <daniel.ray318@gmail.com>
2014-08-13 01:25:37 -0500

    Update bogosort.c

--- bogosort.c
+++ bogosort.c
@@ -1,25 +1,22 @@
 #include <time.h>
 #include <stdlib.h>
 
-void shuffle(int*& arr)
-{
-    int len = sizeof(arr) / sizeof(arr[0]);
-    if (len > 1) 
-    {
-        for (int i = len - 1; i > 0; i--) 
+int main() {
+    srand(time(NULL));
+	int arr[2] = {3,1};
+	int order[2] = {1,3};
+	int len = sizeof(arr) / sizeof(arr[0]);
+        if (len > 1) 
         {
-            int temp = arr[i];
-            int r = rand() % (len + 1);
-            arr[i] = arr[r];
-            arr[r] = temp;
-        }        
-    }
-}
-
-void bogosort(int*& arr, int* order)
-{
-    while (arr != order) 
-    {
-        shuffle(arr);
-    }
+		while (arr != order) 
+		{
+			for (int i = len - 1; i > 0; i--) 
+			{
+				int temp = arr[i];
+				int r = rand() % (len + 1);
+				arr[i] = arr[r];
+				arr[r] = temp;
+			}        
+		}
+	} 
 }
