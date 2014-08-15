d51f6d7c6ae41d283f4b35e9ac7ac7758f187e3f
jdanray <daniel.ray318@gmail.com>
2014-08-15 05:17:42 -0500

    Update bogosort.rb

--- bogosort.rb
+++ bogosort.rb
@@ -1,11 +1,9 @@
 def shuffle(arr)
-    if arr.length > 1
-        i = arr.length - 1
-        while i > 0
-            r = rand(i)
-            arr[i], arr[r] = arr[r], arr[i]
-            i -= 1
-        end
+    i = arr.length - 1
+    while i > 0
+        r = rand(i)
+        arr[i], arr[r] = arr[r], arr[i]
+        i -= 1
     end
     arr
 end
