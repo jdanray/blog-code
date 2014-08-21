83ffb9a58b11ccf77ba5de15c4325995f704413b
jdanray <daniel.ray318@gmail.com>
2014-08-20 20:16:02 -0500

    Create bfs.rb

---
+++ bfs.rb
@@ -0,0 +1,25 @@
+require 'thread'
+
+def match?(t)
+	"fill in with whatever match conditions you want"
+	return false
+end
+
+def bfs(graph, source)
+	q = Queue.new
+	q.push(source)
+	seen = [source]
+	while !q.empty?
+		t = q.pop
+		if match?(t)
+			return t
+		end
+		graph[t].each do |v|
+			if !seen.include?(v)
+				seen.push(v)
+				q.push(v)
+			end
+		end
+	end
+    return nil
+end
