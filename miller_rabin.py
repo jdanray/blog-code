66e1f19ac88fb0957d22b78f68eaaef729716541
jdanray <daniel.ray318@gmail.com>
2015-04-14 19:44:51 -0500

    Update miller_rabin.py

--- miller_rabin.py
+++ miller_rabin.py
@@ -1,16 +1,5 @@
 import random
 
-# given an integer n>1,
-# returns integers k and q such that (2^k)q = n,
-# where k>0 and q is odd
-def kq(n):
-	q = n >> 1 # a right shift is equiv to dividing by 2
-	k = 1
-	while q % 2 == 0:
-		q = q >> 1
-		k += 1
-	return k, q
-
 # given an integer n,
 # returns false if n is not prime
 # returns true if n is likely prime
@@ -21,13 +10,33 @@ def miller_rabin(n):
 		return False
 	elif n == 2 or n == 3:
 		return True
-	elif n % 2 == 0: # no even number >2 is prime
+		
+	# no even number >2 is prime
+	if n % 2 == 0: 
 		return False
-	k, q = kq(n - 1)
-	a = random.randint(2, n - 2) # 1 < a < n-1
+		
+	# find integers k and q such that (2^k)q = n - 1,
+	# where k>0 and q is odd
+	q = (n - 1) >> 1 # a right shift is equiv to dividing by 2
+	k = 1
+	while q % 2 == 0:
+		q = q >> 1
+		k += 1
+	
+	# an integer n>3 is prime if,
+	# for all 1 < a < n - 1,
+	# (a^q) % n = 1 or
+	# there exists some 0 <= j < k such that (a^((2^j)*q)) % n = n - 1
+	# if n is prime, then one of the conditions holds true for all 1 < a < n - 1
+	# so, if it holds true for a random 1 < a < n - 1,
+	# then we have evidence that n is prime
+	a = random.randint(2, n - 2)
 	if (a ** q) % n == 1: 
 		return True
 	for j in range(k):
 		if (a ** ((2 ** j) * q)) % n == n - 1: 
 			return True
+			
+	# neither condition holds true for the random integer
+	# we know that n is not prime
 	return False
