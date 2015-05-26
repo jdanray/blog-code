c73855dfe8013649e6a47b9e0cc4a993ca9c4473
jdanray <daniel.ray318@gmail.com>
2015-05-25 19:58:30 -0500

    Update toposort.py

--- toposort.py
+++ toposort.py
@@ -2,19 +2,19 @@
 # find a linear ordering such that u precedes v
 # for every directed edge uv
 def topo_sort(dag):
-  explored = []
-  sorting = []
+	explored = []
+	sorting = []
 
-  def dfs(u):
-    explored.append(u)
-    if u in dag:
-      for v in dag[u]:
-        if not v in explored:
-				  dfs(v)
-		sorting.insert(0, u)		
+	def dfs(u):
+	explored.append(u)
+ 	if u in dag:
+		for v in dag[u]:
+        		if not v in explored:
+				dfs(v)
+	sorting.insert(0, u)		
 
-  for u in dag:
-    if not u in explored:
-      dfs(u)
+	for u in dag:
+		if not u in explored:
+      			dfs(u)
 	
 	return sorting
