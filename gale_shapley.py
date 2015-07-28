dcdb3c31359e4ea698e88004b28e30a432512c78
jdanray <daniel.ray318@gmail.com>
2015-07-27 22:20:29 -0500

    Update gale_shapley.py

--- gale_shapley.py
+++ gale_shapley.py
@@ -1,30 +1,48 @@
 # given preferences for each man and preferences for each woman,
 # find a stable matching between the men and women
 def gale_shapley(mprefs, wprefs):
+	# test whether there are as many men as women
+	# save the number for future use
+	assert(len(mprefs) == len(wprefs))
+	N = len(mprefs)
+
 	# gale_shapley(mprefs, wprefs) => matching
 	# mprefs[m] is the list of man m's preferences
+	# e.g., if mprefs[0] = [0, 1, 2], then
+	# man 0's preferences are: woman 0 > woman 1 > woman 2
 	# wprefs[w] is the list of woman w's preferences	
 	# matching[w] is the man that woman w has accepted
-	matching = [None] * len(wprefs)
+	matching = [None] * N
+	
+	# if wranking[w][m] = i, then
+	# man m is woman w's i-th choice
+	# (previously i just used wprefs[w].index(m)
+	# but index() is O(n) in python
+	# with this smarter data structure, the algo is faster)
+	wranking = [[None] * N for _ in range(N)]
+	for i, woman in enumerate(wprefs):
+		for j, man in enumerate(woman):
+			wranking[i][man] = j	
 	
 	# if next_woman[m] = w, then 
-	# man m proposes to the wth woman on his list next
+	# man m proposes to the w-th woman on his list next
 	# initially each man proposes to the zeroth (first) woman on his list
-	next_woman = [0] * len(mprefs)
+	next_woman = [0] * N
 	
 	# initially every man is unengaged
-	unengaged_men = range(len(mprefs))
+	unengaged_men = range(N)
 	
 	# until every man is engaged, each unengaged man proposes to the next woman on his list
 	# if the woman is unmatched, match her with the unengaged man
 	# if she is matched but prefers the unengaged man to her fiance,
 	# match her with the unengaged man and make her fiance unengaged
+	# if neither, the man remains unengaged and moves onto the next woman
 	while unengaged_men:
 		man = unengaged_men.pop(0)
 		woman = mprefs[man][next_woman[man]]
 		next_woman[man] += 1
 		if matching[woman]:
-			if wprefs.index(man) < wprefs.index(matching[woman]):				
+			if wranking[woman][man] < wpranking[woman][matching[woman]]:
 				unengaged_men.append(matching[woman])
 				matching[woman] = man
 			else: unengaged_men.append(man)
