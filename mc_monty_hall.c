35960715b2a04bb7b4cb02e5d4b659f0bf280d4e
jdanray <daniel.ray318@gmail.com>
2015-12-01 15:13:35 -0600

    Update mc_monty_hall.c

--- mc_monty_hall.c
+++ mc_monty_hall.c
@@ -4,8 +4,8 @@
 
 int main()
 {
-  const int NUM_DOORS = 3;
-  const int NUM_SIMS = 100000;
+	const int NUM_DOORS = 3;
+	const int NUM_SIMS = 100000;
 	int prize, choice, goat, switch_door, i;
 	int stay_wins = 0, switch_wins = 0;
 
@@ -28,17 +28,17 @@ int main()
 
 
 		// locate other door
-    for (switch_door = 0; switch_door == goat || switch_door == choice; switch_door++);
+    		for (switch_door = 0; switch_door == goat || switch_door == choice; switch_door++);
 
 		// compute wins
 		if (choice == prize)
-      stay_wins++;
-    else if (switch_door == prize)
-      switch_wins++;
+      			stay_wins++;
+    		else if (switch_door == prize)
+      			switch_wins++;
 	}
 
-  printf("%f\n", (1.0 * stay_wins / NUM_SIMS) * 100);
-  printf("%f\n", (1.0 * switch_wins / NUM_SIMS) * 100);
+	printf("%f\n", (1.0 * stay_wins / NUM_SIMS) * 100);
+	printf("%f\n", (1.0 * switch_wins / NUM_SIMS) * 100);
   
-  return 0;
+  	return 0;
 }
