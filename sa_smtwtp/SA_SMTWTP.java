5d0e34402cd7d7204e8f49a962383e2373ba9836
jdanray <daniel.ray318@gmail.com>
2016-01-14 02:11:49 -0600

    Update SA_SMTWTP.java

--- sa_smtwtp/SA_SMTWTP.java
+++ sa_smtwtp/SA_SMTWTP.java
@@ -4,77 +4,80 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class SA_SMTWTP {
-  public static int totalWeightedTardiness(ArrayList<Job> schedule) {
-    int tardiness;
-    int total = 0;
-    int currentTime = 0;
+	public static int totalWeightedTardiness(ArrayList<Job> schedule) {
+    		int tardiness;
+    		int total = 0;
+    		int currentTime = 0;
 
-	  for (Job job: schedule) {
-      currentTime += job.getProcessingTime();
-      tardiness = Math.max(currentTime - job.getDueDate(), 0);
-      total += job.getWeight() * tardiness;
-    }
+	  	for (Job job: schedule) {
+      			currentTime += job.getProcessingTime();
+      			tardiness = Math.max(currentTime - job.getDueDate(), 0);
+      			total += job.getWeight() * tardiness;
+    		}
 	
-	  return total;
-  }
+	  	return total;
+  	}
 
-  public static double acceptanceProbability(int currentTardiness, int neighborTardiness, double temperature) {
-    return Math.exp((currentTardiness - neighborTardiness) / temperature);
-  }
+  	public static double acceptanceProbability(int currentTardiness, int neighborTardiness, double temperature) {
+    		return Math.exp((currentTardiness - neighborTardiness) / temperature);
+  	}
 	
-  public static ArrayList<Job> getNeighbor(ArrayList<Job> currentSchedule) {
-    ArrayList<Job> neighborSchedule;
+  	public static ArrayList<Job> getNeighbor(ArrayList<Job> currentSchedule) {
+    		ArrayList<Job> neighborSchedule;
 
-    // start with a schedule that is exactly the same as the current schedule
-	  neighborSchedule = new ArrayList<Job>();
-	  for (Job job : currentSchedule) {
-      neighborSchedule.add(job);
-	  }
+    		// start with a schedule that is exactly the same as the current schedule
+	  	neighborSchedule = new ArrayList<Job>();
+	  	for (Job job : currentSchedule) {
+      			neighborSchedule.add(job);
+	  	}
 	
-	  // grab two jobs at random
-	  int jobPos1 = (int) (neighborSchedule.size() * Math.random());
-	  int jobPos2 = (int) (neighborSchedule.size() * Math.random());
-	  Job job1 = neighborSchedule.get(jobPos1);
-	  Job job2 = neighborSchedule.get(jobPos2);
+	  	// grab two jobs at random
+	  	int jobPos1 = (int) (neighborSchedule.size() * Math.random());
+	  	int jobPos2 = (int) (neighborSchedule.size() * Math.random());
+	  	Job job1 = neighborSchedule.get(jobPos1);
+	  	Job job2 = neighborSchedule.get(jobPos2);
 		
-	  // swap them
-	  neighborSchedule.set(jobPos1, job2);
-	  neighborSchedule.set(jobPos2, job1);
+	  	// swap them
+	  	neighborSchedule.set(jobPos1, job2);
+	  	neighborSchedule.set(jobPos2, job1);
 	
-	  return neighborSchedule;
-  }
+	  	return neighborSchedule;
+  	}
 
-  public static void main(String[] args) {
-    int currentTardiness;
-    int neighborTardiness;
-	  ArrayList<Job> currentSchedule;
-    ArrayList<Job> neighborSchedule;
+  	public static void main(String[] args) {
+    		int currentTardiness;
+    		int neighborTardiness;
+	  	ArrayList<Job> currentSchedule;
+    		ArrayList<Job> neighborSchedule;
                 
-	  // generate an initial random schedule
-	  currentSchedule = new ArrayList<Job>();
-	  currentSchedule.add(new Job(100, 10, 15));
-	  currentSchedule.add(new Job(25, 5, 1));
-	  currentSchedule.add(new Job(50, 3, 1));
-	  Collections.shuffle(currentSchedule);
+	  	// generate an initial random schedule
+	  	currentSchedule = new ArrayList<Job>();
+	  	currentSchedule.add(new Job(100, 10, 15));
+	  	currentSchedule.add(new Job(25, 5, 1));
+	  	currentSchedule.add(new Job(50, 3, 1));
+	  	Collections.shuffle(currentSchedule);
 		
-	  // get the initial schedule's tardiness
-	  currentTardiness = totalWeightedTardiness(currentSchedule);
+	  	// get the initial schedule's tardiness
+	  	currentTardiness = totalWeightedTardiness(currentSchedule);
 	
-    // parameters
-	  double temperature = 100000;
-	  double coolingRate = 0.9;
+    		// parameters
+	  	double temperature = 100000;
+	  	double coolingRate = 0.9;
 	
-	  while (temperature > 1) {
-      neighborSchedule = getNeighbor(currentSchedule);
-      neighborTardiness = totalWeightedTardiness(neighborSchedule);
-      if (neighborTardiness <= currentTardiness || acceptanceProbability(currentTardiness, neighborTardiness, temperature) > Math.random()) {
-        currentSchedule = neighborSchedule;
-		    currentTardiness = neighborTardiness;
-      }
-      temperature *= coolingRate;
-    }
+	  	while (temperature > 1) {
+      			neighborSchedule = getNeighbor(currentSchedule);
+      			neighborTardiness = totalWeightedTardiness(neighborSchedule);
+      			if (neighborTardiness <= currentTardiness) { 
+           			currentSchedule = neighborSchedule;
+		    		currentTardiness = neighborTardiness;
+      			} else if (acceptanceProbability(currentTardiness, neighborTardiness, temperature) > Math.random()) {
+        			currentSchedule = neighborSchedule;
+		    		currentTardiness = neighborTardiness;
+      			}
+      			temperature *= coolingRate;
+    		}
 		
-	  System.out.println(currentSchedule);
-	  System.out.println(currentTardiness);
-  }
+	  	System.out.println(currentSchedule);
+	  	System.out.println(currentTardiness);
+  	}
 }
