d80e82f05a46d7837e9aefb2416c7d28b01b78b0
jdanray <daniel.ray318@gmail.com>
2016-01-24 14:51:55 -0600

    Update bottom_up_mis.py

--- bottom_up_mis.py
+++ bottom_up_mis.py
@@ -19,12 +19,12 @@ def mis(tree, root):
 	memo = {}
 	for node in bft(tree, root):
 		children = tree[node]
-		ninc = [m for c in children for m in memo[c]]
+		exc = [m for c in children for m in memo[c]]
 		grandchildren = [w for v in children for w in tree[v]]
 		inc = [node] + [m for g in grandchildren for m in memo[g]]
-		memo[node] = max([inc, ninc], key=lambda x: len(x))
+		memo[node] = max([inc, exc], key=lambda x: len(x))
 	return memo[root]
-
+	
 tree = {0: [1], 1: [2, 3, 4], 3: [], 2: [5, 6], 5: [], 6: [], 4: [7, 8], 7: [], 8: []}
 root = 0
 print mis(tree, root)
