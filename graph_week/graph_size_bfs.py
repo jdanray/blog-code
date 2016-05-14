ae0b9584d87922e34e3e41d3fe8fd1f5c5e2e08d
jdanray <daniel.ray318@gmail.com>
2016-05-13 20:50:00 -0500

    Create graph_size_bfs.py

---
+++ graph_week/graph_size_bfs.py
@@ -0,0 +1,66 @@
+INFINITY = 100000
+
+def build_graph(file_location):
+	graph = {}
+	vertices = set()
+	
+	with open(file_location, 'r') as f:
+		next(f)
+		for edge in f:
+			u, v = map(int, edge.split())
+			
+			vertices.add(u)
+			vertices.add(v)
+			
+			if u in graph:
+				graph[u].add(v)
+			else:
+				graph[u] = {v}
+				
+	return graph, vertices
+
+def eccentricity(graph, u):
+	dists = []
+
+	for v in vertices:
+		if u is not v:
+			d = distance(graph, u, v)
+			if d is not None:
+				dists.append(d)
+			
+	if dists:
+		return max(dists)
+	else:
+		return None
+	
+def distance(graph, s, t):
+	dist = {}
+	for u in vertices: 
+		dist[u] = INFINITY
+	dist[s] = 0
+	
+	q = [s]
+	
+	while q:
+		u = q.pop(0)
+		
+		if u == t:
+			return dist[u]
+	
+		if not u in graph:
+			continue
+			
+		for v in graph[u]:
+			if dist[v] == INFINITY:
+				dist[v] = dist[u] + 1
+				q.append(v)
+
+	return None
+	
+file_location = 'graph_size_input.txt'	
+graph, vertices = build_graph(file_location)
+eccentricities = [eccentricity(graph, u) for u in vertices]
+eccentricities = [ecc for ecc in eccentricities if ecc != None]
+radius = min(eccentricities)
+diameter = max(eccentricities)
+print('radius: %i\ndiameter: %i' % (radius, diameter))
