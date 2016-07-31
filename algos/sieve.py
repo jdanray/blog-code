351a895cca0e2acea3c42e630f712cb92aa10a6d
jdanray <daniel.ray318@gmail.com>
2016-07-30 20:46:15 -0500

    added comments

--- algos/sieve.py
+++ algos/sieve.py
@@ -1,15 +1,21 @@
-# the sieve of eratosthenes
-
 def small_prime_list(n):
-	assert 2 <= n <= 2 ** 20
+	assert 2 <= n <= 2 ** 20	
 	
+	# construct all the integers from 2 to n
+	# tag them all as prime initially
 	b = dict((i, True) for i in range(2, n + 1))
 	
+	# iteratively untag the composite numbers
+	# every composite number k has at least one prime factor less than or equal to sqrt(k)
+	# so, to untag all the composite numbers from 2 to n, 
+	# take all the prime numbers from 2 to sqrt(n), and untag all of their multiples
 	i = 2
-	while i ** 2 <= n:
+	while i ** 2 <= n: 
+		# untag all of the multiples of i from 2i, 3i, ..., (n/i)i
 		for j in range(2, (n // i) + 1):
 			b[j * i] = False
 		
+		# go to the next smallest prime number
 		i += 1
 		while not b[i]:
 			i += 1
