40c599bda034d3229c8dd2930959399e28079463
jdanray <daniel.ray318@gmail.com>
2016-08-01 03:26:21 -0500

    Update billboard.py

--- problems/billboard.py
+++ problems/billboard.py
@@ -7,17 +7,23 @@ def max_area_poster(billboards):
 	# return the poster that covers the max total area
 	
 	max_width = max(b[0] for b in billboards)
-	max_height = max(b[1] for b in billboards)	
-	
-	posters = {}
+	max_height = max(b[1] for b in billboards) 
+
+	max_total_area = 0
+	best_poster = None
 	for w in range(1, max_width + 1):
 		for h in range(1, max_height + 1):
-			posters[(w, h)] = 0
+			num_billboards = 0
 			for b in billboards:
 				if w <= b[0] and h <= b[1]:
-					posters[(w, h)] += 1
-	
-	return max(posters, key=lambda p: p[0] * p[1] * posters[p])
+					num_billboards += 1
+			
+			total_area = w * h * num_billboards
+			if total_area > max_total_area:
+				max_total_area = total_area
+				best_poster = (w, h)
+				
+	return best_poster
 	
 billboards = [(1, 4), (2, 2), (4, 1)]
 #billboards = [(1, 4), (2, 2), (3, 3), (4, 4)]
