fd1ea9af8b36d370cc9acea8fcc5170cf3168f6c
jdanray <daniel.ray318@gmail.com>
2016-08-23 17:57:31 -0500

    prettied up

--- csp/min_conflicts.py
+++ csp/min_conflicts.py
@@ -4,17 +4,8 @@ def num_conflicts(variable, value, state, constraints):
 	# constraints is a dictionary of the form
 	# {variable1: [[var_1, relation_1], [var_2, relation_2], ..., [var_n, relation_n]], variable2: ...}
 	# where relation_i is a relation that the given value and the value of var_i must satisfy
-
-	return sum(1 if not relation(variable, value, var, state[var]) else 0 for var, relation in constraints[variable])
-
-	conflicts = 0
 	
-	for cons in constraints[variable]:
-		var, relation = cons
-		if not relation(variable, value, var, state[var]):
-			conflicts += 1  
-
-	return conflicts
+	return sum(1 if not relation(variable, value, var, state[var]) else 0 for var, relation in constraints[variable])
 
 def min_conflicts(csp, max_steps=1000):
 	variables, values, constraints = csp
@@ -31,11 +22,12 @@ def min_conflicts(csp, max_steps=1000):
 
 		variable = choice(conflicting_vars)
 
-		# given the conflicted variable that we've chosen at random,
+		# given the conflicting variable that we've chosen at random,
 		# take the variable value that produces a minimal number of conflicts
 		# if there are multiple such values, pick one at random
 		conflicts = [num_conflicts(variable, val, curr_state, constraints) for val in values[variable]]
-		min_indices = [i for i in range(len(conflicts)) if conflicts[i] == min(conflicts)]
+		min_conf = min(conflicts)
+		min_indices = [i for i, c in enumerate(conflicts) if c == min_conf]
 		index = choice(min_indices)
 		value = values[variable][index]
 		
