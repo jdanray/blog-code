3671a8a2af9b9dcd257af0e10c53c5bce73a30bc
jdanray <jdanray@users.noreply.github.com>
2017-08-15 19:44:43 -0500

    Update simple_env.py

--- ai/qlearn/simple_env.py
+++ ai/qlearn/simple_env.py
@@ -1,13 +1,24 @@
+# In the initial state (State 0), you have two actions available: U and R
+# U rewards you +10 and takes you to State 2, where you have no more actions
+# R costs you -100 and takes you to State 1
+# BUT, in State 1, you can choose R again, which will reward you +122.333333
+# So, even though U gives you +10 and R,R costs you -100,
+# your optimal action sequence is not U, but R,R, because R,R nets you 10.1 in the end
+
 import qlearn
 
 env = qlearn.Environment()
-env.states = {0, 1, 2, 3, 4}
-env.actions = {0: ['U', 'R'], 1: ['D'], 2: ['U', 'R'], 3: ['D'], 4: ['L']}
-env.rewards = {0: {'U': -10, 'R': 0}, 1: {'D': 0}, 2: {'U': -10, 'R': 10}, 3: {'D': 0}, 4: {'L': 0}}
-env.transitions = {0: {'R': 2, 'U': 1}, 1: {'D': 0}, 2: {'U': 3, 'R': 4}, 3: {'D': 2}, 4: {'L': 3}}
+env.states = {0, 1, 2, 3}
+env.actions = {0: ['U', 'R'], 1: ['R'], 2: [], 3: []}
+env.rewards = {0: {'U': 10, 'R': -100}, 1: {'R': 122 + 1/3}, 2: {}, 3: {}}
+env.transitions = {0: {'R': 1, 'U': 2}, 1: {'R': 3}, 2: {}, 3: {}}
 
 agent = qlearn.QLearn(env)
 agent.train()
 
 for s in env.states:
-	print('In state %i, %s is the best action.' % (s, agent.optimal_policy(s)))
+	action = agent.optimal_policy(s)
+	if action:
+		print('In state %i, %s is the best action, and you transition to state %i.' % (s, action, agent.environment.transition(s, action)))
+	else:
+		print('In state %i, you have no actions available.' % (s))
