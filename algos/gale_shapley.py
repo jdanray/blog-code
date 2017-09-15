149e19b138bbbb1ab69b46c9cc8e11498106d75f
jdanray <jdanray@users.noreply.github.com>
2017-09-14 22:44:27 -0500

    fixed logical error, brought up to py3

--- algos/gale_shapley.py
+++ algos/gale_shapley.py
@@ -3,48 +3,47 @@
 def gale_shapley(mprefs, wprefs):
 	# test whether there are as many men as women
 	assert(len(mprefs) == len(wprefs))
-	
-	# mprefs[m] is the list of man m's preferences
-	# e.g., if mprefs[0] = [0, 1, 2], then
-	# man 0's preferences are: woman 0 > woman 1 > woman 2
-	# wprefs[w] is the list of woman w's preferences	
-	# matching[w] is the man that woman w has accepted
-	N = len(mprefs)
+
+	# wprefs[i] is the list of woman i's preferences
+	# e.g., if wprefs[0] == [0, 1, 2], then
+	# woman 0's preferences are: man 0 > man 1 > man 2
+	# matching[i] is the man that woman i has accepted
+	N = len(wprefs)
 	matching = [None] * N
-	
-	# if wchoice[w][m] = i, then
-	# man m is woman w's i-th choice
-	# (previously I just used wprefs[w].index(m)
-	# but index() is O(n) in python
-	# with this smarter data structure, the algo is faster)
-	wchoice = [[None] * N for _ in range(N)]
-	for w, woman in enumerate(wprefs):
-		for m, man in enumerate(woman):
-			wchoice[w][man] = m
-	
-	# if next_woman[m] = w, then 
-	# man m proposes to the w-th woman on his list next
+
+	# if wrank[i][j] == k, then
+	# man j is woman i's k-th choice
+	wrank = [[None] * N for _ in range(N)]
+	for i, wp in enumerate(wprefs):
+		for k, j in enumerate(wp):
+			wrank[i][j] = k
+
+	# if next_woman[i] == j, then 
+	# man i proposes to the j-th woman on his list next
 	# initially each man proposes to the zeroth (first) woman on his list
 	next_woman = [0] * N
-	
+
 	# initially every man is unengaged
 	# until every man is engaged, each unengaged man proposes to the next woman on his list
 	# if the woman is unmatched, match her with the unengaged man
 	# if she is matched but prefers the unengaged man to her fiance,
 	# match her with the unengaged man and make her fiance unengaged
 	# if neither, the man remains unengaged and moves onto the next woman
-	unengaged_men = range(N)
+	unengaged_men = list(range(N))
 	while unengaged_men:
 		man = unengaged_men.pop(0)
 		woman = mprefs[man][next_woman[man]]
 		next_woman[man] += 1
-		if matching[woman]:
-			if wchoice[woman][man] < wchoice[woman][matching[woman]]:
-				unengaged_men.append(matching[woman])
-				matching[woman] = man
-			else: unengaged_men.append(man)
-		else: matching[woman] = man
+		if matching[woman] == None:
+			matching[woman] = man
+		elif wrank[woman][man] < wrank[woman][matching[woman]]:
+			unengaged_men.append(matching[woman])
+			matching[woman] = man
+		else:
+			unengaged_men.append(man)
 
 	return matching
-	
-print gale_shapley([[0, 1, 2], [2, 1, 0], [1, 0, 2]], [[1, 2, 0], [2, 1, 0], [0, 1, 2]])
+
+mprefs = [[0, 1], [0, 1]]
+wprefs = [[0, 1], [0, 1]]
+print(gale_shapley(mprefs, wprefs)) # output: [0, 1]
