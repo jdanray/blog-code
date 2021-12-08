9e6ad4525204e48bce397d4d91d82abd1a96f98e
Dan <jdanray@users.noreply.github.com>
2021-12-08 14:53:23 -0600

    id3/voting.py: refactored a bit

--- ai/id3/voting.py
+++ ai/id3/voting.py
@@ -1,21 +1,17 @@
 from id3 import id3
-from collections import Counter
+from collections import Counter, defaultdict
+
+train_file = 'vote_train.txt'
+test_file = 'vote_test.txt'
+
+target = 'party'
+
+features = ["infants", "water_share", "budget_res", "physician_fee_freeze", "aid", "religion", "satellite_ban", "contras", "missile", "immigration", "corp_cutback", "education", "sue", "crime", "exports", "africa", "party"]
 
 def preprocess(file_location):
 	examples = []
-	with open(file_location, 'r') as f:
-		for example in f:
-			infants, water_share, budget_res, physician_fee_freeze, aid, \
-			religion, satellite_ban, contras, missile, immigration, \
-			corp_cutback, education, sue, crime, exports, africa, party = example.rstrip().split(',')
-			
-			examples.append({'infants': infants, 'water_share': water_share, \
-			'budget_res': budget_res, 'physician_fee_freeze': physician_fee_freeze, \
-			'aid': aid, 'religion': religion, 'satellite_ban': satellite_ban, \
-			'contras': contras, 'missile': missile, 'immigration': immigration, \
-			'corp_cutback': corp_cutback, 'education': education, 'sue': sue, 'crime': crime, \
-			'exports': exports, 'africa': africa, 'party': party})
-			
+	with open(file_location, 'r') as file:
+		examples = [dict(zip(features, ex.rstrip().split(','))) for ex in file]
 	return examples
 
 # given a list of test examples, the target attribute, and a decision tree,
@@ -30,19 +26,17 @@ def test_tree(tree, examples, target):
 				attr_val = c.most_common(1)[0][0]
 			tree = tree[attr][attr_val]			
 		return tree
-	
 	return Counter([classify(e, tree) == e[target] for e in examples])
 
-train_file = 'vote_train.txt'
-training = preprocess(train_file)
+def main():
+	training = preprocess(train_file)
+	testing = preprocess(test_file)
+	attributes = [a for a in training[0].keys() if a is not target]
 
-test_file = 'vote_test.txt'
-testing = preprocess(test_file)
+	tree = id3(training, attributes, target)
 
-target = 'party'
-attributes = [a for a in training[0].keys() if a is not target]
+	results = test_tree(tree, testing, target)
+	print('%f%% correct' % (results[True] * 100.0 / len(testing)))
 
-tree = id3(training, attributes, target)
-
-results = test_tree(tree, testing, target)
-print('%f%% correct' % (results[True] * 100.0 / len(testing)))
+if __name__ == "__main__":
+	main()
